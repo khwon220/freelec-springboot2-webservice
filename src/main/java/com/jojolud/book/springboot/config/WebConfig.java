a628494cb462bc5e7639af8d63311e8258f6b0a6
heewonkim <khwon220@gmail.com>
2022-06-24 18:00:43 +0900

    ~chapter5.4

---
+++ src/main/java/com/jojolud/book/springboot/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.jojolud.book.springboot.config;
+
+import com.jojolud.book.springboot.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    /**
+     * 생성한 LoginUserArgumentResolver가 스프링에서 인식될 수 있도록 WebMvcConfigurer에 추가
+     */
+
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+    /**
+     * HandlerMethodArgumentResolver는 항상 WebMvcConfigurer의 addArgumentResolvers()를 통해 추가해야 함
+     * 다른 HandlerMethodArgumentResolver가 필요하다면 같은 방식으로 추가해 주면 됨
+     */
+}
--- src/main/java/com/jojolud/book/springboot/web/IndexController.java
+++ src/main/java/com/jojolud/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojolud.book.springboot.web;
 
+import com.jojolud.book.springboot.config.auth.LoginUser;
 import com.jojolud.book.springboot.config.auth.dto.SessionUser;
 import com.jojolud.book.springboot.service.posts.PostsService;
 import com.jojolud.book.springboot.web.dto.PostsResponseDto;
@@ -9,24 +10,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController { // 페이지에 관련된 컨트롤러는 모두 IndexController를 사용
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+//    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
+        /** @LoginUser SessionUser user
+         * 기존에 (User) httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선됨
+         * 이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보를 가져올 수 있게 됨
+         */
+
         model.addAttribute("posts", postsService.findAllDesc());
         /** Model
          * 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
          * postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달
          */
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         /** (SessionUser) httpSession.getAttribute("user");
          * 앞서 작성된 CustomerOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
          * 즉, 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음
